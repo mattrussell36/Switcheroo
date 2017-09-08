ad4eb34231ab71fd7768e5a6fd94489b28373d26
Matthew Russell <matthewrussell@Matthews-MacBook-Pro.local>
2017-09-08 02:59:15 +0100

    adding webpack manifest html and copy plugins to manage indext.html and assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const PATHS = {
+    dist: path.join(__dirname, 'dist'),
+    src: path.join(__dirname, 'src'),
+}
 
 module.exports = {
     entry: {
@@ -8,7 +15,7 @@ module.exports = {
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: PATHS.dist,
     },
     devtool: 'source-map',
     module: {
@@ -37,5 +44,26 @@ module.exports = {
         alias: {
             'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
         }
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(PATHS.src, 'index.html'),
+            excludeChunks: [
+                'background'
+            ]
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: path.join(PATHS.src, 'styles'),
+                to: path.join(PATHS.dist, 'styles'),
+            },
+            {
+                from: path.join(PATHS.src, 'images'),
+                to: path.join(PATHS.dist, 'images')
+            }
+        ]),
+        new ManifestPlugin({
+            seed: require('./src/manifest.json'),
+        }),
+    ]
 }
